fea043f5d3168e0b310e116b2d27f68f712dd723
mickaelyoshua <mickael_yoshua@hotmail.com>
2019-10-30 10:28:48 -0300

    Update somador_completo.vhd

--- somador_completo.vhd
+++ somador_completo.vhd
@@ -18,3 +18,32 @@ begin
         S<=temp xor Cin;
         Cout<=(A and B) or (temp and Cin);
 end ci;
+
+
+
+entity testbench is
+end testbench;
+
+architecture test of testbench is
+
+component somador_completo is
+        port(
+                A, B, Cin: in bit;
+                S, Cout: out bit
+        );
+  end component;
+
+signal inputs: bit_vector(2 downto 0):= "000";
+signal outputs: bit_vector(1 downto 0);
+
+begin
+        DUT: somador_completo port map(inputs(0), inputs(1), inputs(2), outputs(0), outputs(1));
+        
+        inputs<="001" after 10 ns,
+                          "010" after 20 ns,
+                          "011" after 30 ns,
+                          "100" after 40 ns,
+                          "101" after 50 ns,
+                          "110" after 60 ns,
+                          "111" after 70 ns;
+end test;
